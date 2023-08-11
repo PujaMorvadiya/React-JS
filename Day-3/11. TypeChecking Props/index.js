882bd4d76f6c4827c43e9b2a4a8a4ced89bcd914
Puja Moravadiya <pujamorvadiya1242@gmail.com>
2023-08-11 20:21:31 +0530

    Delete Day-3/11. TypeChecking Props directory

--- Day-3/11. TypeChecking Props/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Student from "./Student";
-
-// Rendering Component
-ReactDOM.render(
-  <Student name="Rahul" roll="101" />,
-  document.getElementById("root")
-);
