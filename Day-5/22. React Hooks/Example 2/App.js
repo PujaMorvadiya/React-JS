22f42a1d4695db5daef9b47d63506af830f1640e
Puja Moravadiya <pujamorvadiya1242@gmail.com>
2023-08-13 12:14:17 +0530

    Add files via upload

---
+++ Day-5/22. React Hooks/Example 2/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("Rahul");
+  // const name = nameStateVariable[0];
+  // const setName = nameStateVariable[1];
+  const handleClick = () => {
+    setName("Maya");
+  };
+  return (
+    <React.Fragment>
+      <h1> {name} </h1>{" "}
+      <button type="button" onClick={handleClick}>
+        Change{" "}
+      </button>{" "}
+    </React.Fragment>
+  );
+}
+export default App;
